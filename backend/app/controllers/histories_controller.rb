c29101820e0c49226358e3af434a76ed5310037f
seireitou <ausis@mail.com>
2021-06-06 22:29:37 -0500

    sets up histories controller

--- backend/app/controllers/histories_controller.rb
+++ backend/app/controllers/histories_controller.rb
@@ -1,2 +1,17 @@
 class HistoriesController < ApplicationController
+    def index 
+        histories = History.all
+        render json: histories
+    end
+
+    def create
+        history = History.new(history_params)
+        if history.save
+            render json: comment
+    end
+
+
+    def history_params
+        params.require(:history).permit(:total, :length, :date, :paid)
+    end
 end
