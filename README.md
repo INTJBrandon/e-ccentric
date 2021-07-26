ec003e411beb9e776620cd5dd5a00c6b9095166e
seireitou <ausis@mail.com>
2021-07-25 19:43:24 -0500

    adds readme file

---
+++ README.md
@@ -0,0 +1,34 @@
+# Welcome to E-Ccentric
+
+E-ccentric is a telsa charging simulation app that allows users to "charge" their Tesla while using the web application. There's an estimation form so users can know how much they will pay before select the service. On the start charging page users may "charge" their tesla while watching seeing how much they are being charged and how long. After it's ended they may choose to pay now or later (feature not added) and then check that transaction on the transaction page.
+
+## Summary
+
+## Installing and Running The Program
+The way to use this program is to fork and clone this github repository 
+Once the github repository is on your system open it with any code editor
+Run the following snippets of code to get everything set up and running for the program
+
+    bundle install
+This will install the neccessary ruby gems onto your machine.
+To start the RAILS API, type the following into your terminal to execute the application
+
+    rails s
+This will start the server so that the API can be accessed and then simply open the index.html file
+
+    npm install 
+This will install the neccessary NPM front-end requirements. To start the react server you must then type
+
+    npm start
+This will start the react server, type 'y' when it asks if you would like to run the server on another port.
+
+## Authors
+ *Brandon Tyus*
+    [Brandon Tyus](https://github.com/INTJBrandon)
+
+## License
+
+This project is licensed under the [CC0 1.0 Universal](LICENSE.md)
+Creative Commons License - see the [LICENSE.md](LICENSE.md) file for
+details
+
--- frontend/src/footer.js
+++ frontend/src/footer.js
@@ -1,19 +1,19 @@
 export default function Footer(props) {
     return (
-        <footer class="bg-dark text-center text-white footer">
-            <div class="container p-4 pb-0">
-                <section class="mb-4">
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/brandon-tyus-0b7134152/" target="_blank" rel="noreferrer" role="button"
-                    ><i class="fab fa-linkedin-in"></i
+        <footer className="bg-dark text-center text-white footer">
+            <div className="container p-4 pb-0">
+                <section className="mb-4">
+                <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/brandon-tyus-0b7134152/" target="_blank" rel="noreferrer" role="button"
+                    ><i className="fab fa-linkedin-in"></i
                 ></a>
-                <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/INTJBrandon" target="_blank" rel="noreferrer" role="button"
-                    ><i class="fab fa-github"></i
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/INTJBrandon" target="_blank" rel="noreferrer" role="button"
+                    ><i className="fab fa-github"></i
                 ></a>
                 </section>
             </div>
-            <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+            <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
                 © 2020 Brandon Tyus |  
-                <a class="text-white" href="/home" style={{textDecoration: "none"}}>  E-ccentric</a>
+                <a className="text-white" href="/home" style={{textDecoration: "none"}}>  E-ccentric</a>
             </div>
         </footer>
     )
